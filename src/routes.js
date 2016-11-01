c0f60456bd1a079a84f6c54c7150b244f77251e8
Kelvin Mijaya <kelvinmijaya@gmail.com>
2016-11-01 17:20:39 +0700

    fix prod setting
    - fixing production setting

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import { Route, IndexRoute } from 'react-router'
 // your current file is.
 import CoreLayout from './layouts/CoreLayout/CoreLayout'
 import HomeView from './module/home/views/HomeView'
+
 export default (store) => (
     <Route path='/' component={CoreLayout}>
       <IndexRoute component={HomeView} />
--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import api from '../middleware/api'
-import rootReducer from '../reducers'
+import rootReducer from '../reducers/rootReducer'
 
 const configureStore = preloadedState => createStore(
   rootReducer,
   preloadedState,
-  applyMiddleware(thunk, api)
+  applyMiddleware(thunk)
 )
 
 export default configureStore
