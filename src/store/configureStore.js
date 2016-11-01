afdbf42c9d19d2b6e6962c2c8e7062d242529ed0
yansen armandau <yansenarmandau.dev@gmail.com>
2016-11-01 18:32:57 +0700

    make it one file for configure store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,35 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
-} else {
-  module.exports = require('./configureStore.dev')
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+import rootReducer from '../reducers/rootReducer'
+import DevTools from '../containers/DevTools'
+
+const configureStore = preloadedState => {
+
+  const middleware = [thunk]
+  const enhancers = []
+  if (process.env.NODE_ENV === 'development') {
+    middleware.push(createLogger())
+    enhancers.push(DevTools.instrument())
+  }
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    compose(
+      applyMiddleware(...middleware),
+      ...enhancers
+    )
+  )
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers/rootReducer', () => {
+      const nextRootReducer = require('../reducers/rootReducer').default
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
+  return store
 }
+
+export default configureStore
